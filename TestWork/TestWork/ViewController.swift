13c3e75c003321c83b47b68f8cdd526e9fbd7a11
Наташа Ананас <konnat7@mail.ru>
2023-03-07 19:41:59 +0300

    Шаблон

---
+++ TestWork/TestWork/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TestWork
+//
+//  Created by Анастасия Козлова on 07.03.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
